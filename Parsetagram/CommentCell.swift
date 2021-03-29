3632cbead619ddf0999bdb4998209181efd3ffdb
Minsol Jeong <jminsol0525@gmail.com>
2021-03-29 14:29:47 -0700

    display comments

---
+++ Parsetagram/CommentCell.swift
@@ -0,0 +1,28 @@
+//
+//  CommentCell.swift
+//  Parsetagram
+//
+//  Created by Minsol Jeong on 2021/03/29.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
